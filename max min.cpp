d613647d39190abcc9d8c3f673bb1a43df4d9a61
scriptpi01 <88918870+scriptpi01@users.noreply.github.com>
2022-03-08 02:13:31 +0700

    Add files via upload

---
+++ max min.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int i,min,max,n,a;
+    min = 2000000000; max = -2000000000;
+    cin >> a;
+    for (i = 1; i <= a; i++) {
+        cin >> n ;
+        if (n > max) {
+            max = n;
+        }
+        if (n < min) {
+            min = n;
+        }
+    }
+    cout << min << endl;
+    cout << max;
+
+}
